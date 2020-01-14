ebe7cc1ef3d6cf69f0198775ce8d92360783f639
Swapnil Gawande <swapnil.gawande@nihilent.com>
2020-01-14 10:21:42 +0530

    Home activity UI changes

--- app/src/main/java/com/swapnilgawande/helloworld/HomeActivity.java
+++ app/src/main/java/com/swapnilgawande/helloworld/HomeActivity.java
@@ -1,14 +1,49 @@
 package com.swapnilgawande.helloworld;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+    EditText etName;
+    EditText etPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        etName = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+
+        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+
+    }
+
+    private void doLogin() {
+        String name = etName.getText().toString();
+        String password = etPassword.getText().toString();
+
+        show(name, password);
+    }
+
+    void show(String name, String password) {
+        Log.d(TAG, "name " + name + " password " + password);
+    }
+
+    public int getNumber() {
+        return 10;
     }
 }
