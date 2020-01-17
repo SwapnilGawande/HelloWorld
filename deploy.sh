b2a1f4e4f1560c268e4b364e7262d78e43bbbae2
Swapnil Gawande <swapnil.gawande@nihilent.com>
2020-01-17 12:28:59 +0530

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 curl \
 -F "status=2" \
 -F "notify=1" \
--F "ipa=@${2:-app/build/outputs/apk/debug/app-debug.apk}" \
+-F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
 -H "X-HockeyAppToken:ff47c175b55cfbab58837bb66656b50a186ab06d" \
 https://rink.hockeyapp.net/api/2/apps/HelloWorld/app_versions/upload
