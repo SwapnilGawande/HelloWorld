7f3569f0b1b64b10ac8637d08412957e26670677
Swapnil Gawande <swapnil.gawande@nihilent.com>
2020-01-17 11:31:48 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,6 @@
-
-AndroidAppId=${1:-ff47c175b55cfbab58837bb66656b50a186ab06d}
-HockeyAppToken=9297faf4da2a149a865bb60c5d42f175e95c1207
-TAGS=${3:-}
-
 curl \
 -F "status=2" \
--F "notify=0" \
--F "notes_type=1" \
--F "notes=%23+$CIRCLE_BRANCH%0D%0A$CIRCLE_SHA1%0D%0A$CIRCLE_REPOSITORY_URL" \
--F "repository_url=$CIRCLE_REPOSITORY_URL" \
--F "build_server_url=$CIRCLE_BUILD_URL" \
--F "commit_sha=$CIRCLE_SHA1" \
--F "ipa=@${2:-app/build/outputs/apk/app-debug.apk}" \
--F "tags=$TAGS" \
--H "X-HockeyAppToken: $HockeyAppToken" \
-https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
+-F "notify=1" \
+-F "ipa=@${2:-app/build/outputs/apk/debug/app-debug.apk}" \
+-H "X-HockeyAppToken:ff47c175b55cfbab58837bb66656b50a186ab06d" \
+https://rink.hockeyapp.net/api/2/apps/HelloWorld/app_versions/upload
