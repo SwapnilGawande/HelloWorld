37d9304a8ddd5914920419da8337e446eec03f91
Swapnil Gawande <swapnil.gawande@nihilent.com>
2020-01-17 16:55:46 +0530

    Updated scripts

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ curl \
 -F "notes=CircleCI" \
 -F "notify=1" \
 -F "ipa=/home/circleci/project/outputs/outputs/apk/debug/app-debug.apk" \
--H "X-HockeyAppToken:9297faf4da2a149a865bb60c5d42f175e95c1207" \
+-H "X-HockeyAppToken: 1c20e700a28c8f45a6f9f760f512baf53888d3e7" \
 https://rink.hockeyapp.net/api/2/apps/f6e0bf05-8c6d-4287-b9f4-77826b34686a/app_versions/
