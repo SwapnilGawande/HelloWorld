f925f422188c1b7d4b2a1ce13b7b276ddadadb40
Swapnil Gawande <swapnil.gawande@nihilent.com>
2020-01-17 14:56:17 +0530

    Added note

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 curl \
 -F "status=2" \
+-F "notes=CircleCI" \
 -F "notify=1" \
 -F "ipa=/home/circleci/project/outputs/outputs/apk/debug/app-debug.apk" \
 -H "X-HockeyAppToken:ff47c175b55cfbab58837bb66656b50a186ab06d" \
