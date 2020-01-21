51041f676dbb66fa8eb326d0d1e4c66a08e5e3bc
Swapnil Gawande <swapnil.gawande@nihilent.com>
2020-01-21 15:56:35 +0530

    updated script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-curl --verbose \
-     --fail \
-     --form "status=2" \
-     --form "ipa=/Users/swapnil/Swapnil/DevOps/HelloWorld/app/build/outputs/apk/debug/app-debug.apk" \
-     --header "X-HockeyAppToken: d7f1a450e8fa60fc0e2696a2cfb829a511562b33" \
-https://rink.hockeyapp.net/api/2/apps/b1d55578-6e0e-47fa-a058-47ba30d78a10/app_versions
+curl \
+-F "status=2" \
+-F "notes=CircleCI" \
+-F "notify=1" \
+-F "ipa=/home/circleci/project/outputs/outputs/apk/debug/app-debug.apk" \
+-H "X-HockeyAppToken:39dc03e52b423663dadae939b76d4506eedd00d7" \
+"https://rink.hockeyapp.net/api/2/apps/b1d55578-6e0e-47fa-a058-47ba30d78a10/app_versions"
